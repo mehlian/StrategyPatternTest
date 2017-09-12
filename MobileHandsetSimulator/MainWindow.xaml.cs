4d85a9d46ad979853e53eed046d44059fc8c78e6
mehlian <xpilarz0@gmail.com>
2017-09-12 16:41:58 +0200

    added basic button implementation.

--- MobileHandsetSimulator/MainWindow.xaml.cs
+++ MobileHandsetSimulator/MainWindow.xaml.cs
@@ -23,6 +23,55 @@ namespace MobileHandsetSimulator
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            var nokiaLumia920 = new NokiaLumia920();
+            var nokia5510 = new Nokia5510();
+            cmbboxPhoneModel.ItemsSource = new[] { nokia5510 };
+
+        }
+
+        private void btnCall_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = string.Empty;
+            var nokia6600 = new Nokia6600();
+            MainOutputWindow.Text = nokia6600.MakeACall("666");
+        }
+
+        private void btnSMS_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = string.Empty;
+            var nokia6600 = new Nokia6600();
+            MainOutputWindow.Text = nokia6600.SendSms("test sms");
+        }
+
+        private void btnActivateFrontCamera_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = "Activate front camera";
+            var nokia6600 = new Nokia6600();
+            nokia6600.LoadFrontCamera();
+        }
+
+        private void btnActivateRearCamera_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = "Activate rear camera";
+            var nokia6600 = new Nokia6600();
+            nokia6600.LoadRearCamera();
+        }
+
+        private void btnTakeSnap_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = string.Empty;
+            var nokia6600 = new Nokia6600();
+            MainOutputWindow.Text = nokia6600.TakeSnap(@"I:\snap...");
+        }
+
+        private void btnRecordVideo_Click(object sender, RoutedEventArgs e)
+        {
+            MainOutputWindow.Text = string.Empty;
+            var nokia6600 = new Nokia6600();
+            MainOutputWindow.Text = nokia6600.RecordVideo(@"I:\record...");
         }
     }
 }
